4f9e770d0ee12c5998eb0f9aa462bba024b76fe1
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-03-21 21:43:37 +0300

    Display comments.
    I created addComments.js and getComments.js files inside the module directory.

---
+++ modules/getComments.js
@@ -0,0 +1,22 @@
+const projectID = '0PsaHQATvBBc06xhH6ES';
+const baseLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+
+const getComments = async (index) => {
+  const get = await fetch(`${baseLink}${projectID}/comments?item_id=item${index}`);
+  const arr = await get.json();
+  const empty = await arr.error;
+  const div = document.getElementById('divComments');
+  div.innerHTML = '';
+  if (empty === undefined) {
+    arr.forEach((element) => {
+      const p = document.createElement('p');
+      p.className = 'listOfComment';
+      p.innerHTML = `${element.creation_date}  ${element.username}: ${element.comment}`;
+      div.appendChild(p);
+    });
+  }
+};
+
+export default getComments;
+
+export { projectID, baseLink };
